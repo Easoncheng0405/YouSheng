2577f83132215ceeebe4077cf86a54a5b3cbdb22
zhaoyang10 <zhaoyang10@xiaomi.com>
2018-10-15 19:02:40 +0800

    import Alibaba ARouterwq

--- app/src/main/java/com/yousheng/yousheng/Application.java
+++ app/src/main/java/com/yousheng/yousheng/Application.java
@@ -1,14 +1,17 @@
 package com.yousheng.yousheng;
 
+import com.alibaba.android.arouter.launcher.ARouter;
+
 import org.litepal.LitePal;
 import org.litepal.LitePalApplication;
 
 public class Application extends LitePalApplication {
 
-    //初始化litepal配置
     @Override
     public void onCreate() {
         super.onCreate();
+        //初始化litepal配置
         LitePal.initialize(this);
+        ARouter.init(this);
     }
 }
